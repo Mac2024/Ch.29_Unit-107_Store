3cdebbf9987b227b8a0fd59f6c6b88220179b797
Mac2024 <99700613+Mac2024@users.noreply.github.com>
2022-06-23 18:50:26 -0500

    Add files via upload

---
+++ src/components/footer.jsx
@@ -0,0 +1,10 @@
+import "./footer.css"
+
+function Footer(){
+    return (
+        <div className="footer">
+            <p> This is a footer &copy; 2022 Mario McGrady</p>
+        </div>);
+}
+
+export default Footer;
